47d020df3b19cf4fc71cc17e12d9dad4c6480b64
Aaron Wickman <wickmanaaron@gmail.com>
2017-09-16 12:03:14 -0400

    Added facebook sdk and login, needs to run outside of windows

--- js/postbot3000.js
+++ js/postbot3000.js
@@ -1,28 +1,32 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '110446862999562',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v2.10'
-  });
-  FB.AppEvents.logPageView();
-};
+$(document).ready(function() {
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-///End FB Init
+  ///FB SDK INIT
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId            : '110446862999562',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+    $('#loginbutton,#feedbutton').removeAttr('disabled');
+    FB.getLoginStatus(updateStatusCallback);
 
-//Get Random Integer
- function getRandomInt(min, max) {
-     return Math.floor(Math.random() * (max - min + 1)) + min
- }
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  ///End FB Init
 
-$(function(){
+  //Get Random Integer
+   function getRandomInt(min, max) {
+       return Math.floor(Math.random() * (max - min + 1)) + min
+   }
+
   //Generate Sentence Parts and Store Them
   var pronoun = getRandomInt(0,pronouns.length);
   pronoun = pronouns[pronoun];
@@ -51,4 +55,5 @@ $(function(){
     $('.string').html(string);
   });
 
-})
+});
+});
